8de64290d082d5069dd454cde023c2a41193e22d
Hendricck <105647636+Hendricck-Anthony@users.noreply.github.com>
2022-08-16 18:08:43 -0300

    Feat - added button in card component

--- src/app/components/SimpleCard.jsx
+++ src/app/components/SimpleCard.jsx
@@ -13,11 +13,14 @@ const CardTitle = styled('div')(({ subtitle }) => ({
   marginBottom: !subtitle && '16px',
 }));
 
-const SimpleCard = ({ children, title, subtitle, icon }) => {
+const SimpleCard = ({ children, title, subtitle, icon, button }) => {
   return (
     <CardRoot elevation={6}>
-      <CardTitle subtitle={subtitle}>{title}</CardTitle>
-      {subtitle && <Box sx={{ mb: 2 }}>{subtitle}</Box>}
+      <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+        <CardTitle subtitle={subtitle}>{title}</CardTitle>
+        {subtitle && <Box sx={{ mb: 2 }}>{subtitle}</Box>}
+        {button}
+      </div>
       {children}
     </CardRoot>
   );
