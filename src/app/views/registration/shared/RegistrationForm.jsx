a4b133343911b3c452f81cd16c89437eaedfcc6f
eric-lino-coder <ericwillian2018@gmail.com>
2023-01-23 16:46:06 -0300

    FEAT: added address in the edit form

--- src/app/views/registration/shared/RegistrationForm.jsx
+++ src/app/views/registration/shared/RegistrationForm.jsx
@@ -3,8 +3,10 @@ import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import {
+    AppBar,
     Button,
     Grid,
+    Toolbar,
 } from '@mui/material'
 import { styled } from '@mui/system'
 import React, { useState, useEffect } from 'react'
@@ -14,68 +16,78 @@ import { DatePicker } from '@mui/lab'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import { get } from 'lodash'
+import { H2} from 'app/components/Typography'
 
-export default function RegistrationForm(props) {const [open, setOpen] = React.useState(false)
+const TextField = styled(TextValidator)(() => ({
+    width: '100%',
+    marginBottom: '16px',
+}))
 
-    const TextField = styled(TextValidator)(() => ({
-        width: '100%',
-        marginBottom: '16px',
-    }))
-        
-        const [dialogState,setDialogState]=useState("formulario")
-        const initialState = {
-            monthStart:'',
-            email:'',
-            name:'',
-            status:'',
+export default function RegistrationForm(props) {
+    const [open, setOpen] = useState(false)
+
+    const [matAddress, setMatAddress] = useState('')
+    const [matComplement, setMatComplement] = useState('')
+    const [matNumber, setMatNumber] = useState('')
+    const [matCity, setMatCity] = useState('')
+    const [matDistrict, setMatDistrict] = useState('')
+    const [matCep, setMatCep] = useState('')
+    const [matUf, setMatUf] = useState('')
+   
+    const [dialogState,setDialogState]=useState("formulario")
+    const initialState = {
+        monthStart:'',
+        email:'',
+        name:'',
+        status:'',
+    }
+    const [state, setState] = useState({
+        initialState,
+        monthStart: new Date(),
+    })
+    const handleDateChange = (monthStart) => {
+        setState({ ...state, monthStart })
+    }
+    const {
+        monthStart: monthStart,
+        email: email,
+        name: name,
+        status: status,
+    } = state
+
+    const handleSubmit = async (event) => {
+        const data =     
+        {
+            mat_created_at:  monthStart,
+            mat_email: email,
+            mat_name: name,
+            mat_status: status,
+            mat_address: matAddress,
+            mat_address_complement: matComplement,
+            mat_address_number: matNumber,
+            mat_city: matCity,
+            mat_district: matDistrict,
+            mat_postal_code: matCep,
+            mat_uf: matUf
         }
-        const [state, setState] = useState({
-            initialState,
-            monthStart: new Date(),
+        try {
+            setDialogState("carregando")
+            const response = await axios.post("/api/v1/registration/createregistration",data)
+            setDialogState("sucesso")
+            props.onSubmit()
+        } catch (error) {
+            console.log("erro ao efetuar matricula", error)
+            setDialogState("erro")
+        }
+    }
+
+    const handleChange = (event) => {
+        event.persist()
+        setState({
+            ...state,
+            [event.target.name]: event.target.value,
         })
-        const handleDateChange = (monthStart) => {
-            setState({ ...state, monthStart })
-        }
-        const {
-            monthStart: monthStart,
-            email: email,
-            name: name,
-            status: status,
-        } = state
-    
-        const handleSubmit = async (event) => {
-            const data =     
-            {
-                mat_created_at: monthStart,
-                mat_email: email,
-                mat_name: name,
-                mat_status: status,
-                mat_address: matAddress,
-                mat_address_complement: matComplement,
-                mat_address_number: matNumber,
-                mat_city: matCity,
-                mat_district: matDistrict,
-                mat_postal_code: matCep,
-                mat_uf: matUf
-            }
-            try {
-                setDialogState("carregando")
-                const response = await axios.post("/api/v1/registration/createregistration",data)
-                setDialogState("sucesso")
-                props.onSubmit()
-            } catch (error) {
-                console.log("erro ao efetuar matricula", error)
-                setDialogState("erro")
-            }
-        }
-    
-        const handleChange = (event) => {
-            event.persist()
-            setState({
-                ...state,
-                [event.target.name]: event.target.value,
-            })
-        }
+    }
 
     function handleClickOpen() {
         setOpen(true)
@@ -100,16 +112,6 @@ export default function RegistrationForm(props) {const [open, setOpen] = React.u
         }
     };
     
-
-    const [matAddress, setMatAddress] = useState('')
-    const [matComplement, setMatComplement] = useState('')
-    const [matNumber, setMatNumber] = useState('')
-    const [matCity, setMatCity] = useState('')
-    const [matDistrict, setMatDistrict] = useState('')
-    const [matCep, setMatCep] = useState('')
-    const [matUf, setMatUf] = useState('')
-
-    
     switch (dialogState) {
         case "formulario":
             
@@ -119,17 +121,31 @@ export default function RegistrationForm(props) {const [open, setOpen] = React.u
                 onClose={handleClose}
                 aria-labelledby="form-dialog-title"
                 maxWidth="md"
+
                 
             >
+                 <AppBar sx={{ position: 'relative' }}>
+                    <Toolbar>
+                        <H2
+                            sx={{
+                                flex: 1,
+                                marginLeft:2,
+                            }}
+                        >
+                            CADASTRO
+                        </H2>
+                    </Toolbar>
+                </AppBar>
+                <DialogTitle id="form-dialog-title">Dados Básicos</DialogTitle>
                 <ValidatorForm
                     onSubmit={handleSubmit}
                     onError={() => null}
                     autoComplete="no"
                 >
-            <DialogTitle id="form-dialog-title">Dados Básicos</DialogTitle>
+
             <DialogContent>
-            <Grid container spacing={1} marginTop>
-                <Grid item lg={6} md={6} sm={12} xs={12}>
+                <Grid container spacing={1} marginTop>
+                    <Grid item lg={6} md={6} sm={12} xs={12}>
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                             <DatePicker
                                 value={monthStart}
@@ -146,7 +162,7 @@ export default function RegistrationForm(props) {const [open, setOpen] = React.u
                                 )}
                             />
                         </LocalizationProvider>
-                </Grid>
+                    </Grid>
                 <Grid item lg={6} md={6} sm={12} xs={12} >
                     <TextValidator
                         label="Email"
--- src/app/views/registration/shared/RegistrationFormEdit.jsx
+++ src/app/views/registration/shared/RegistrationFormEdit.jsx
@@ -13,6 +13,9 @@ import axios from '../../../../axios'
 import { DatePicker } from '@mui/lab'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import { H2 } from 'app/components/Typography'
+import Toolbar from '@mui/material/Toolbar'
+import AppBar from '@mui/material/AppBar'
 
 export default function RegistrationFormEdit(props) {
     
@@ -24,7 +27,25 @@ export default function RegistrationFormEdit(props) {
     const [name, setName] = useState('')
     const [status, setStatus] = useState('')
 
-
+    const [matAddress, setMatAddress] = useState('')
+    const [matComplement, setMatComplement] = useState('')
+    const [matNumber, setMatNumber] = useState('')
+    const [matCity, setMatCity] = useState('')
+    const [matDistrict, setMatDistrict] = useState('')
+    const [matCep, setMatCep] = useState('')
+    const [matUf, setMatUf] = useState('') 
+    const [dialogState,setDialogState]=useState("formulario")
+    
+    const [state, setState] = useState({
+        monthStart: new Date(),
+    })
+    const handleDateChange = (monthStart) => {
+        setState({ ...state, monthStart })
+    }
+    const {
+        monthStart: monthStart
+    } = state
+   
     useEffect(() => {
         async function getUser(){
             
@@ -33,40 +54,58 @@ export default function RegistrationFormEdit(props) {
             setEmail(response.data.mat_email)
             setName(response.data.mat_name)
             setStatus(response.data.mat_status)
+            setMatCep(response.data.mat_postal_code)
+            setMatAddress(response.data.mat_address)
+            setMatCity(response.data.mat_city)
+            setMatDistrict(response.data.mat_district)
+            setMatUf(response.data.mat_uf)
+            setMatNumber(response.data.mat_address_number)
+            setMatComplement(response.data.mat_address_complement)
         }
         getUser();
     },[props.update])
-        
-        const [dialogState,setDialogState]=useState("formulario")
-        
-        const [state, setState] = useState({
-            monthStart: new Date(),
-        })
-        const handleDateChange = (monthStart) => {
-            setState({ ...state, monthStart })
-        }
-        const {
-            monthStart: monthStart
-        } = state
     
-        const handleSubmit = async (event) => {
-            const data =     
-            {
-                mat_created_at: monthStart,
-                mat_email: email,
-                mat_name: name,
-                mat_status: status,
-            }
-            try {
-                setDialogState("carregando")
-                const response = await axios.put("/api/v1/registration/"+rid,data)
-                setDialogState("sucesso")
-                props.onSubmit()
-            } catch (error) {
-                console.log("erro ao efetuar matricula", error)
-                setDialogState("erro")
-            }
+    const searchCep = async (e) => {
+        const cep = e.target.value.replace(/\D/g, '');
+        setMatCep(cep)
+        
+        if (cep.length === 8) {
+        
+            const response = await axios.get('https://viacep.com.br/ws/'+cep+'/json')
+            const data = response.data
+            setMatAddress(data.logradouro);
+            setMatCity(data.localidade);
+            setMatDistrict(data.bairro);
+            setMatUf(data.uf);
         }
+    };
+    
+
+    const handleSubmit = async (event) => {
+        const data =     
+        {
+            mat_created_at:  monthStart,
+            mat_email: email,
+            mat_name: name,
+            mat_status: status,
+            mat_address: matAddress,
+            mat_address_complement: matComplement,
+            mat_address_number: matNumber,
+            mat_city: matCity,
+            mat_district: matDistrict,
+            mat_postal_code: matCep,
+            mat_uf: matUf
+        }
+        try {
+            setDialogState("carregando")
+            const response = await axios.put("/api/v1/registration/"+rid,data)
+            setDialogState("sucesso")
+            props.onSubmit()
+        } catch (error) {
+            console.log("erro ao efetuar matricula", error)
+            setDialogState("erro")
+        }
+    }
 
     function handleClose() {
         setOpen(false)
@@ -83,7 +122,19 @@ export default function RegistrationFormEdit(props) {
                 maxWidth="md"
                 
             >
-                <DialogTitle id="form-dialog-title">Cadastro</DialogTitle>
+                 <AppBar sx={{ position: 'relative' }}>
+                    <Toolbar>
+                        <H2
+                            sx={{
+                                flex: 1,
+                                marginLeft:2,
+                            }}
+                        >
+                            ATUALIZAR
+                        </H2>
+                    </Toolbar>
+                </AppBar>
+                <DialogTitle id="form-dialog-title">Dados Básicos</DialogTitle>
                 <ValidatorForm
                     onSubmit={handleSubmit}
                     onError={() => null}
@@ -156,6 +207,102 @@ export default function RegistrationFormEdit(props) {
             </Grid>
         
                 </DialogContent>
+            <DialogTitle id="form-dialog-title">Endereço</DialogTitle>
+            <DialogContent>
+            <Grid container spacing={1} marginTop>
+                <Grid item lg={2} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="CEP"
+                        onChange= {searchCep}
+                        name="cep"
+                        type="text"
+                        value={matCep || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item lg={6} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="Logradouro (Ex.: Rua...)"
+                        onChange= {e => setMatAddress(e.target.value)}
+                        name="address"
+                        type="text"
+                        value={matAddress || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item lg={2.5} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="Bairro"
+                        onChange= {e => setMatDistrict(e.target.value)}
+                        name="district"
+                        type="text"
+                        value={matDistrict || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item lg={1.5} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="Número"
+                        onChange= {e => setMatNumber(e.target.value)}
+                        name="number"
+                        type="text"
+                        value={matNumber || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item lg={4} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="Complemento"
+                        onChange= {e => setMatComplement(e.target.value)}
+                        name="complement"
+                        type="text"
+                        value={matComplement || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item lg={4} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="Estado"
+                        onChange= {e => setMatUf(e.target.value)}
+                        name="estate"
+                        type="text"
+                        value={matUf || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item lg={4} md={6} sm={12} xs={12} >
+
+                    <TextValidator
+                        label="Cidade"
+                        onChange= {e => setMatCity(e.target.value)}
+                        name="city"
+                        type="text"
+                        value={matCity || ''}
+                        validators={['required']}
+                        errorMessages={['Este campo é obrigatório']}
+                        fullWidth
+                    />
+                </Grid>
+            </Grid>
+            </DialogContent>
                 <DialogActions>
                     <Button
                         color="primary"
@@ -169,7 +316,7 @@ export default function RegistrationFormEdit(props) {
                     type="submit"
                     color="primary"
                     >
-                    Cadastrar
+                    Atualizar
                     </Button>
                 </DialogActions>
                 </ValidatorForm>
